d49ae79fa8123176a046e68e685fe13f413ad2c9
LuckyLarryLive <LuckyLarry7128@gmail.com>
2025-05-08 03:16:17 -0400

    fix: update WalletProvider import in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,17 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { WalletContextProvider } from "./contexts/WalletContext";
+import { WalletProvider } from "./contexts/WalletContext";
 
 createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <WalletContextProvider>
-      <App />
-    </WalletContextProvider>
-  </QueryClientProvider>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <WalletProvider>
+        <App />
+      </WalletProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
